9a7cf3f741b5daec71744566f0983c979cfdcb5c
Charlie Coppinger <charles.coppinger@gmail.com>
2024-02-10 17:31:25 +1300

    meta tags and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,34 @@
 	import '../app.pcss';
 </script>
 
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>RoastMyUX — Product & UX/UI design feedback service</title>
+	<meta name="title" content="RoastMyUX — Product & UX/UI design feedback service" />
+	<meta
+		name="description"
+		content="Get quick, actionable feedback on your product by having your designs roasted by a professional."
+	/>
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://roastmyux.com/" />
+	<meta property="og:title" content="RoastMyUX — Product & UX/UI design feedback service" />
+	<meta
+		property="og:description"
+		content="Get quick, actionable feedback on your product by having your designs roasted by a professional."
+	/>
+	<meta property="og:image" content="https://roastmyux.com/images/meta-cover.png" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://roastmyux.com/" />
+	<meta property="twitter:title" content="RoastMyUX — Product & UX/UI design feedback service" />
+	<meta
+		property="twitter:description"
+		content="Get quick, actionable feedback on your product by having your designs roasted by a professional."
+	/>
+	<meta property="twitter:image" content="https://roastmyux.com/images/meta-cover.png" />
+</svelte:head>
+
 <slot />
